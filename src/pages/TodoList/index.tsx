b51db5a488ca4bfd0e31a06679aef52db9d32f29
Jahongirhacking <Jahongirhacking@gmail.com>
2024-02-11 01:31:07 +0500

    init: create TodoList page

--- src/pages/TodoList/index.tsx
+++ src/pages/TodoList/index.tsx
@@ -1,17 +1,22 @@
+import { useSelector } from "react-redux";
 import AddTaskButton from "../../components/TodoList/AddTaskButton";
 import TaskInput from "../../components/TodoList/TaskInput";
 import "./style.scss";
+import { IStore } from "../../types/store";
+import TaskList from "../../components/TodoList/TaskList";
 
 const TodoList = () => {
+    const { todos, activeTask } = useSelector((state: IStore) => state.todos)
+    const { showAddTaskButton } = useSelector((state: IStore) => state.todos)
     return (
         <div className="todo-list">
             <header>
-                <h2><span>#1</span> Time to focus!</h2>
+                <h2><span>#1</span> {activeTask.name}</h2>
                 <div className="list-head">Tasks</div>
             </header>
-            <ul></ul>
-            <TaskInput />
-            <AddTaskButton />
+            <TaskList todos={todos.filter(todo => !todo.completed)} />
+            <TaskList todos={todos.filter(todo => todo.completed)} />
+            {showAddTaskButton ? <AddTaskButton /> : <TaskInput />}
         </div>
     )
 }
